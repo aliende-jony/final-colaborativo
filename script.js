05c1a1eaf22c835c753d3b99ed68e5dd88014742
aliende jonatan <jonsitoaliende@gmail.com>
2023-04-06 16:40:02 -0300

    ult act

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ fetch("./database.json")
     for (const producto of productos) {
         contac.innerHTML += `
         <div>
-           <h2>${producto.name}</h2>
-           <h2>${producto.modelos}</h2>
-           <p>${producto.price}</p>
-        
+        <h2>${producto.name}</h2>
+        <h2>${producto.modelos}</h2>
+        <p>${producto.price}</p>
         `
     }
 })
