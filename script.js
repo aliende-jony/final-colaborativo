e3b5386796055aa794d05c0a25dcca7e0780ef44
aliende jonatan <jonsitoaliende@gmail.com>
2023-04-11 10:47:37 -0300

    m11/4

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-fetch("./database.json")
-.then(Response => Response.json())
 
-.then(data =>{
-    for (const producto of productos) {
-        contac.innerHTML += `
-        <div>
-        <h2>${producto.name}</h2>
-        <h2>${producto.modelos}</h2>
-        <p>${producto.price}</p>
-        `
-    }
+
+
+const formularioHTML = document.getElementById("form")
+formularioHTML.addEventListener("submit", (event ) =>{
+    event.preventDefault()
+    console.log(event)
+    console.log(formularioHTML.name.value)
+    console.log(formularioHTML.correoElectronico.value)
+    formularioHTML.reset()
 })
+
+fetch("./productos.json")
+    .then(response => response.json())
+    .then(data => {
+
+        for(const producto of data){
+            contenedor.innerHTML += `
+            <div>
+                <h2>${producto.name}</h2>
+                <p>Price: $${producto.price}</p>
+            </div>
+            `
+        }
+    })
+
